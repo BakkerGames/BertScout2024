6f655e8aeba793376bf16d84a4c5b10ff8533f23
Scott Bakker <Scott@MeowPC>
2024-02-02 18:15:17 -0500

    adb_get_database.bat

--- Data2024/adb_get_database.bat
+++ Data2024/adb_get_database.bat
@@ -1,5 +1,7 @@
 @echo off
-adb shell getprop ro.boot.serialno>serialno.txt
+"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe" shell getprop ro.boot.serialno>serialno.txt
 set /p SERIALNO=<serialno.txt
-adb pull /sdcard/Documents/BertScout2024.db3 "%SERIALNO%.db3"
+"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe" pull /sdcard/Documents/BertScout2024.db3 "%SERIALNO%.db3"
 del serialno.txt
+echo Copied to %SERIALNO%.db3
+pause
