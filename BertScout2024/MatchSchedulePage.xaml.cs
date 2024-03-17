15d4416edf900deebb4f4b3c32c59133aff8f979
Scott Bakker <sbakker343@gmail.com>
2024-03-17 12:07:31 -0400

    thinking...

--- BertScout2024/MatchSchedulePage.xaml.cs
+++ BertScout2024/MatchSchedulePage.xaml.cs
@@ -1,9 +1,17 @@
+using BertScout2024.Databases;
+using BertScout2024.Models;
+
 namespace BertScout2024;
 
 public partial class MatchSchedulePage : ContentPage
 {
+    private readonly LocalDatabase db = new();
+
+    private MatchPreload matchPreload = new();
+
 	public MatchSchedulePage()
 	{
 		InitializeComponent();
-	}
+		MatchesPreloadView.ItemsSource = matchPreload.PreloadList;
+    }
 }
